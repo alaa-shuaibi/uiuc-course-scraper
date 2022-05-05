3b8aee3c1ee1725993dfba4554f2f2d19c7d4f9d
Alaa Shuaibi <55153524+shuaibi27@users.noreply.github.com>
2022-05-05 08:14:48 -0500

    changed how to take in CL arguments

--- scrape.py
+++ scrape.py
@@ -1,11 +1,12 @@
-import argparse, requests, json
+import sys, requests, json
 from bs4 import BeautifulSoup
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--subject")
-args = parser.parse_args()
+if len(sys.argv) != 2:
+    raise Exception('Exactly 1 argument is required: <SUBJECT>')
 
-data = requests.get('http://catalog.illinois.edu/courses-of-instruction/' + str(args.subject))
+subject = sys.argv[1]
+
+data = requests.get('http://catalog.illinois.edu/courses-of-instruction/' + subject)
 
 if data.status_code >= 400:
     print('Status Code: ' + str(data.status_code))
@@ -19,8 +20,8 @@ for courseblock in container.find_all('div', {'class': 'courseblock'}):
     title = courseblock.find('p', {'class': 'courseblocktitle'}).text
     desc = courseblock.find('p', {'class': 'courseblockdesc'}).text.replace('\xa0', ' ').replace('\n', '')
 
-    name = title[:len(str(args.subject)) + 4].replace('\xa0', ' ')
-    label = title[len(str(args.subject)) + 7 : title.find('credit:') - 3]
+    name = title[:len(str(subject)) + 4].replace('\xa0', ' ')
+    label = title[len(str(subject)) + 7 : title.find('credit:') - 3]
     hours = title[title.find('credit:') + 8 : -2]
 
     key = name.replace(' ', '').lower()
@@ -34,5 +35,5 @@ for courseblock in container.find_all('div', {'class': 'courseblock'}):
     # print(name + ' | ' + label + ' | ' + hours)
     # print(desc + '\n')
 
-with open(args.subject + '.json', 'w') as outfile:
+with open("data/" + subject + '.json', 'w') as outfile:
     json.dump(courses, outfile)
